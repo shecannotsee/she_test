45763a794365cc67a882bd067f245c8a715a52a3
shecannotsee <shecannotsee@163.com>
2024-02-04 16:30:27 +0800

    [feat] 1.add test and exec code

---
+++ exec/application.cpp
@@ -0,0 +1,10 @@
+//
+// Created by shecannotsee on 24-2-4.
+//
+#include <iostream>
+
+int main() {
+  std::cout << "application start.\n";
+  std::cout << "application Done.\n";
+  return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by shecannotsee on 24-2-4.
+//
+#include <iostream>
+
+#include "global_test_set.h"
+
+int main() {
+  std::cout << GREEN_COLOR << "test start.\n" << RESET_COLOR;
+  std::cout << GREEN_COLOR << "test Done.\n" << RESET_COLOR;
+  return 0;
+}
