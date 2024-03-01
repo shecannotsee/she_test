40ec7d98ab1966887c96e6cbf23a9eb1b49ddaf5
shecannotsee <shecannotsee@163.com>
2024-03-01 16:55:02 +0800

    [feat] 1.Add const char* color

--- src/print_color.h
+++ src/print_color.h
@@ -5,12 +5,18 @@
 
 namespace she_test {
 namespace print_color {
-const std::string RESET_COLOR  = "[0m";
-const std::string RED_COLOR    = "[31m";
-const std::string GREEN_COLOR  = "[32m";
-const std::string YELLOW_COLOR = "[33m";
-const std::string PURPLE_COLOR = "[35m";
-const std::string BLUE_COLOR   = "[34m";
+const char* RESET_COLOR          = "[0m";
+const char* RED_COLOR            = "[31m";
+const char* GREEN_COLOR          = "[32m";
+const char* YELLOW_COLOR         = "[33m";
+const char* PURPLE_COLOR         = "[35m";
+const char* BLUE_COLOR           = "[34m";
+const std::string RESET_COLOR_S  = "[0m";
+const std::string RED_COLOR_S    = "[31m";
+const std::string GREEN_COLOR_S  = "[32m";
+const std::string YELLOW_COLOR_S = "[33m";
+const std::string PURPLE_COLOR_S = "[35m";
+const std::string BLUE_COLOR_S   = "[34m";
 }  // namespace print_color
 }  // namespace she_test
 
