616028e808f0fd400cbbc45360f057f99a50473e
shecannotsee <shecannotsee@163.com>
2024-05-07 15:39:31 +0800

    [test] Add integrity test

---
+++ test/integrity_test/main.h
@@ -0,0 +1,19 @@
+#ifndef INTEGRITY_TEST_MAIN_H
+#define INTEGRITY_TEST_MAIN_H
+
+#include "../test_support.h"
+#include "test_case_registry/register_center.h"
+
+namespace integrity_test {
+
+inline int run_test(int argc, char** argv) {
+  test_support _("integrity_test");
+
+  she_test::details::register_center::init_and_run(argc, argv);
+
+  return 0;
+}
+
+}  // namespace integrity_test
+
+#endif  // INTEGRITY_TEST_MAIN_H
--- test/main.cpp
+++ test/main.cpp
@@ -4,15 +4,17 @@
 #include "register_demo_test/main.h"
 
 #include "command_line_parse_test/main.h"
+#include "integrity_test/main.h"
 #include "output_format_test/main.h"
 #include "template_support_test/start_test.h"
 #include "test_case_registry_test/main.h"
 
-int main() {
+int main(int argc, char** argv) {
   command_line_parse_test::run_test();
   easy_demo_test::run_test();
   output_format_test::run_test();
   template_support_test::run_test();
   test_case_registry_test::run_test();
+  integrity_test::run_test(argc,argv);
   return 0;
 }
