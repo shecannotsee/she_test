42598b2036025004f921c1535ef4667c722b5c55
shecannotsee <shecannotsee@163.com>
2024-05-07 10:44:07 +0800

    [test] Check the output information.Luckily, it works

--- test/test_case_registry_test/case1.cpp
+++ test/test_case_registry_test/case1.cpp
@@ -3,3 +3,27 @@
 SHE_TEST(suite1, case1) {
   return true;
 }
+
+SHE_TEST(suite1, case2) {
+  return true;
+}
+
+SHE_TEST(suite1, case3) {
+  return true;
+}
+
+SHE_TEST(suite2, case1) {
+  return true;
+}
+
+SHE_TEST(suite2, case2) {
+  return true;
+}
+
+SHE_TEST(suite2, case3) {
+  return true;
+}
+
+SHE_TEST(suite2, case_false) {
+  return false;
+}
