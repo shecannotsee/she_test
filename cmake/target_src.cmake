ee5d30fcd44ff9c2b7ae5ecf67e78ba0fd88a2ab
shecannotsee <shecannotsee@163.com>
2024-11-21 15:51:33 +0800

    [build] 1.install keep dir

--- cmake/target_src.cmake
+++ cmake/target_src.cmake
@@ -17,18 +17,3 @@ if (build_shared_libs STREQUAL ON)
     set_target_properties(${library_dynamic_name} PROPERTIES OUTPUT_NAME ${project_name})
     target_link_libraries(${library_dynamic_name} PRIVATE ${src_link_dependency})
 endif ()
-
-# Easy to install
-set(include_list
-        ${CMAKE_SOURCE_DIR}/src/command_line/options.h
-        ${CMAKE_SOURCE_DIR}/src/command_line/parser.h
-        ${CMAKE_SOURCE_DIR}/src/format/color.h
-        ${CMAKE_SOURCE_DIR}/src/format/fmt.h
-        ${CMAKE_SOURCE_DIR}/src/format/gtest.h
-        ${CMAKE_SOURCE_DIR}/src/format/style_v1.h
-        ${CMAKE_SOURCE_DIR}/src/format/template_format.h
-        ${CMAKE_SOURCE_DIR}/src/test_case_registry/multi_level_test_case.h
-        ${CMAKE_SOURCE_DIR}/src/test_tools/equal.h
-        ${CMAKE_SOURCE_DIR}/src/she_test.h
-        ${CMAKE_SOURCE_DIR}/src/run_all_test.h
-)
