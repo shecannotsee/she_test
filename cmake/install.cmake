cb2f5570f19bf233579ed54026c1b2f18a5ad7da
shecannotsee <shecannotsee@163.com>
2024-11-21 15:59:57 +0800

    [build] 1.fix install path

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -3,47 +3,47 @@ if (NOT DEFINED CMAKE_INSTALL_PREFIX)
     set(CMAKE_INSTALL_PREFIX "/")
 endif ()
 
+# Ensure proper install paths using GNUInstallDirs for standardized locations
+include(GNUInstallDirs)
+
 # Install header files while maintaining directory structure
 install(DIRECTORY ${CMAKE_SOURCE_DIR}/src/
-        DESTINATION ${project_name}/include
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
         FILES_MATCHING
         PATTERN "*.h"
 )
+
 # Install static library
 install(TARGETS
         ${library_static_name}
-        DESTINATION
-        ${project_name}/lib
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR} # For static libraries
 )
+
 # Install dynamic library
 if (build_shared_libs STREQUAL ON)
     install(TARGETS
             ${library_dynamic_name}
-            DESTINATION
-            ${project_name}/lib
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} # For shared libraries
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} # For Windows DLLs, if applicable
     )
 endif ()
 
 # Install other module
 install(TARGETS
         ${other_name}
-        DESTINATION
-        ${project_name}/bin
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} # For executables
 )
 
 # Install testing module
 if (build_tests STREQUAL ON)
     install(TARGETS
             ${test_name}
-            DESTINATION
-            ${project_name}/bin # install path
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} # For test executables
     )
 endif ()
 
-# Install cmake file
+# Install cmake configuration file
 #install(FILES
 #        ${CMAKE_SOURCE_DIR}/cmake/${project_name}-config.cmake
-#        DESTINATION
-#        ${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/cmake
+#        DESTINATION ${CMAKE_INSTALL_DATADIR}/${project_name}/cmake
 #)
-
