1b36036f260d5fb83ca05508677e9d9f998dec07
shecannotsee <shecannotsee@163.com>
2024-11-21 12:35:54 +0800

    [test] 1.Modify test case

--- test/functional/case1.cpp
+++ test/functional/case1.cpp
@@ -2,34 +2,43 @@
 
 #include <iostream>
 
+// level.test
+// test1
+// level1.test
+// level1.level2.test
+// level1.level2.level3.test
+// level1.level2.level3.level4.test
+// level1.level2.level3.level4.level5.test
+// level1.level2.level3.level4.level5.level6.test
+
 TEST(level, test) {
-  std::cout << "level/test" << std::endl;
+  std::cout << "level.test" << std::endl;
 }
 
 TEST1(test1) {
-  std::cout << "/test" << std::endl;
+  std::cout << "test" << std::endl;
 }
 
 TEST2(level1, test) {
-  std::cout << "/level1/test" << std::endl;
+  std::cout << "level1.test" << std::endl;
 }
 
 TEST3(level1, level2, test) {
-  std::cout << "/level1/level2/test" << std::endl;
+  std::cout << "level1.level2.test" << std::endl;
 }
 
 TEST4(level1, level2, level3, test) {
-  std::cout << "/level1/level2/level3/test" << std::endl;
+  std::cout << "level1.level2.level3.test" << std::endl;
 }
 
 TEST5(level1, level2, level3, level4, test) {
-  std::cout << "/level1/level2/level3/level4/test" << std::endl;
+  std::cout << "level1.level2.level3.level4.test" << std::endl;
 }
 
 TEST6(level1, level2, level3, level4, level5, test) {
-  std::cout << "/level1/level2/level3/level4/level5/test" << std::endl;
+  std::cout << "level1.level2.level3.level4.level5.test" << std::endl;
 }
 
 TEST7(level1, level2, level3, level4, level5, level6, test) {
-  std::cout << "/level1/level2/level3/level4/level/level6/test" << std::endl;
+  std::cout << "level1.level2.level3.level4.level.level6.test" << std::endl;
 }
