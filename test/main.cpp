5c0e07e72142c27c1aba5374326d1bb7bf775910
shecannotsee <shecannotsee@163.com>
2024-11-21 10:16:13 +0800

    [refactor] 1.Rename

--- test/main.cpp
+++ test/main.cpp
@@ -3,7 +3,7 @@
 #include "command_line_parse_test/main.h"
 #include "format/main.h"
 #include "functional/main.h"
-#include "test_tools/start_test.h"
+#include "test_tools/main.h"
 
 int main() {
   easy_demo_test::run_test();
