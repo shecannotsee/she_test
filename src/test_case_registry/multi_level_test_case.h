ac3e1cca6b878d0da7e6e7590f5ead4981f53e82
shecannotsee <shecannotsee@163.com>
2024-11-21 16:07:54 +0800

    [fix] 1.fix marco

--- src/test_case_registry/multi_level_test_case.h
+++ src/test_case_registry/multi_level_test_case.h
@@ -232,9 +232,9 @@ class multi_level_test_case {
   void __func(bool& __pass);                                                                                           \
   struct __register {                                                                                                  \
     __register() {                                                                                                     \
-      multi_level_test_case::add({#name1, #name2, #name3, #name4, #name5, #name6, #name7, #name8, #name9, #name10,     \
-                                  #name11, #name12, #name13, #name14, #name15},                                        \
-                                 __func);                                                                              \
+      multi_level_test_case::add_test_case({#name1, #name2, #name3, #name4, #name5, #name6, #name7, #name8, #name9,    \
+                                            #name10, #name11, #name12, #name13, #name14, #name15},                     \
+                                           __func);                                                                    \
     }                                                                                                                  \
   } __register_func;                                                                                                   \
   }                                                                                                                    \
